c2db7ee87fbecc35aa1cc8a625f3f3018c41d623
kayongo brian <kayonbrian@gmail.com>
2021-08-20 11:59:55 +0300

    restored the db

--- contact_form_mail/settings.py
+++ contact_form_mail/settings.py
@@ -73,23 +73,17 @@ WSGI_APPLICATION = 'contact_form_mail.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         'USER': os.environ.get('DB_USER', 'postgres'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#     }
-# }
 
 
 # Password validation
